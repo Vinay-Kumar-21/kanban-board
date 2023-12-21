67e5734e4ebed570ca7faf5a47f176080642e275
Vinay Kumar <vinaykumarraju21@gmail.com>
2023-12-21 21:41:26 +0530

    add & del functionalities added

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let addBtn = document.querySelector(".add-btn");
-let modalCont = document.querySelector(".modal-cont");
+let addBtn = document.querySelector(".add-btn");//select add btn
+let modalCont = document.querySelector(".modal-cont");//select modal container
+let delBtn = document.querySelector(".remove-btn");//select remove btn
+let textArea = document.querySelector("textarea");//select text area
+let mainContainer = document.querySelector(".main-cont");//select container
 let addModal = true;
+let delBtnClicked = false;
 
 addBtn.addEventListener('click', function () {
     if (addModal) {
@@ -11,3 +15,40 @@ addBtn.addEventListener('click', function () {
     }
     addModal = !addModal;
 })
+delBtn.addEventListener('click', function () {
+    if (!delBtnClicked) {
+        delBtn.style.color = 'red';
+        alert('click on ticket it will delete until bin is red');
+    }
+    else {
+        delBtn.style.color = 'black';
+    }
+    delBtnClicked = !delBtnClicked;
+})
+
+textArea.addEventListener('keydown', function (e) {
+    let key = e.key;
+    if (key == "Enter") {
+        //generate ticket
+        createTicket();
+        modalCont.style.display = 'none'
+
+        //empty txt area value
+        textArea.value = "";
+
+    }
+})
+
+function createTicket() {
+    let tickCont = document.createElement("div");
+    tickCont.className = "ticket-cont";
+    tickCont.innerHTML = '<div class="ticket-color"></div> <div class="ticket-id">#rtyhj</div> <div class="ticket-area">Some task</div>';
+    mainContainer.appendChild(tickCont);
+
+    tickCont.addEventListener('click', function () {
+        if (delBtnClicked) {
+            tickCont.remove();
+        }
+
+    })
+}
